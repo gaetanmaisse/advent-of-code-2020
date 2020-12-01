3431877e77c5a8c538bfedd1defa002513cd236a
Gaëtan Maisse <gmaisse@solocal.com>
2020-12-01 20:50:05 +0100

    Day 1 - Part 2

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -1,17 +1,39 @@
-import { processExpenseReport } from './day1';
+import {
+  processExpenseReportWith2Numbers,
+  processExpenseReportWith3Numbers,
+} from './day1';
 import { input } from './day1.input';
 
 describe('Day1', () => {
-  describe('finds the two entries that sum to 2020 and then multiply those two numbers together', () => {
+  describe('processExpenseReportWith2Numbers', () => {
+    describe('finds the 2 entries that sum to 2020 and then multiply those two numbers together', () => {
+      it('for the example', () => {
+        const exampleInput = [1721, 979, 366, 299, 675, 1456];
+        const exampleResult = 514579;
+
+        expect(processExpenseReportWith2Numbers(exampleInput)).toBe(
+          exampleResult,
+        );
+      });
+
+      it('for the official input', () => {
+        expect(processExpenseReportWith2Numbers(input)).toBe(299299);
+      });
+    });
+  });
+
+  describe('finds the 3 entries that sum to 2020 and then multiply those two numbers together', () => {
     it('for the example', () => {
       const exampleInput = [1721, 979, 366, 299, 675, 1456];
-      const exampleResult = 514579;
+      const exampleResult = 241861950;
 
-      expect(processExpenseReport(exampleInput)).toBe(exampleResult);
+      expect(processExpenseReportWith3Numbers(exampleInput)).toBe(
+        exampleResult,
+      );
     });
 
     it('for the official input', () => {
-      expect(processExpenseReport(input)).toBe(299299);
+      expect(processExpenseReportWith3Numbers(input)).toBe(287730716); // ?.
     });
   });
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,8 +1,22 @@
-export function processExpenseReport(input: number[]): number {
+export function processExpenseReportWith2Numbers(input: number[]): number {
   for (let i = 0; i < input.length; i++) {
     for (let j = i; j < input.length; j++) {
       if (input[i] + input[j] === 2020) {
-        return input[i] * input[j]; // ?.
+        return input[i] * input[j];
+      }
+    }
+  }
+
+  return 0;
+}
+
+export function processExpenseReportWith3Numbers(input: number[]): number {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      for (let k = j + 1; k < input.length; k++) {
+        if (input[i] + input[j] + input[k] === 2020) {
+          return input[i] * input[j] * input[k];
+        }
       }
     }
   }
